76266131dae2d29166eaec02539458f9bccd9978
bwireman <bwireman1@gmail.com>
2022-07-01 13:04:00 -0500

    clean up

--- index.ts
+++ index.ts
@@ -1,31 +1,29 @@
-import fs from "fs"
+import { readFile } from "fs/promises"
 import { spawnSync } from "child_process"
-import process from "process"
-import toml from "toml"
-import path from "path"
+import { cwd, chdir } from "process"
+import { parse } from "toml"
+import { basename } from "path"
 import { Plugin, PluginBuild, Message } from "esbuild"
 
 const PLUGIN_NAME = "esgleam"
 
-const convertMessage = (message: string): Message => ({ pluginName: PLUGIN_NAME, location: null, notes: [], detail: null, text: `\n${PLUGIN_NAME}:\n${message}` })
+const convertMessage = (message: string): Message => ({ id: message, pluginName: PLUGIN_NAME, location: null, notes: [], detail: null, text: `\n${PLUGIN_NAME}:\n${message}` })
 
 const compile = (gleam_dir: string, extra_args: string[]) => {
-    const pwd: string = process.cwd()
-    process.chdir(gleam_dir)
+    const pwd = cwd()
+    chdir(gleam_dir)
     const gleam = spawnSync("gleam", ["build", "--target", "javascript", ...extra_args])
 
-    if (process.cwd() !== pwd) {
-        process.chdir("..")
+    if (cwd() !== pwd) {
+        chdir(pwd)
     }
 
     return { out: gleam.stdout, stderr: gleam.stderr, err: gleam.error }
 }
 
 const load_gleam_name = async (gleam_dir: string): Promise<string> => {
-    const contents = await fs.promises.readFile(`${gleam_dir}/gleam.toml`, "utf8")
-    const data = toml.parse(contents)
-    const name = data.name
-    return name
+    const contents = await readFile(`${gleam_dir}/gleam.toml`, "utf8")
+    return parse(contents).name
 }
 
 export interface EsGleamOptions { project_root?: string, main_function?: string, compile_args?: string[] }
@@ -37,8 +35,8 @@ export function esgleam(opts: EsGleamOptions | undefined = undefined): Plugin {
                 const project_root = opts?.project_root ?? "."
                 const compile_args = opts?.compile_args ?? []
 
-                const filename: string = path.basename(args.path).replace(".gleam", ".mjs")
-                const project_name: string = await load_gleam_name(project_root)
+                const filename = basename(args.path).replace(".gleam", ".mjs")
+                const project_name = await load_gleam_name(project_root)
                 const build_path = `${project_root}/build/dev/javascript/${project_name}/dist`
 
                 const { out: _, stderr, err } = compile(project_root, compile_args)
@@ -50,7 +48,7 @@ export function esgleam(opts: EsGleamOptions | undefined = undefined): Plugin {
                     return { errors: [convertMessage(`Error running gleam: ${err.message}`)] }
                 }
 
-                let contents = await fs.promises.readFile(`${build_path}/${filename}`, "utf8")
+                let contents = await readFile(`${build_path}/${filename}`, "utf8")
                 if (opts?.main_function !== undefined) {
                     contents += `\n\n${opts.main_function}();`
                 }
